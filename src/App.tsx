9777cc51f3b03d8629738568a723efda115e2f68
vdaCodebytz <vishnu.3b@gmail.com>
2024-01-09 21:45:38 +0530

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/Pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import SigninForm from "./_auth/forms/SigninForm";
+
 const App = () => {
-  return <div className="text-center">App</div>;
+  return (
+    <main>
+      {/* Public Routes */}
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="/auth/sign_up" element={<SignupForm />} />
+          <Route path="/auth/sign_in" element={<SigninForm />} />
+        </Route>
+
+        {/* Private Routes */}
+
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 export default App;
